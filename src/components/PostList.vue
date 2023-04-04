2ac4f4cd3250b768f2780b2d88d1d4ac84b01a61
EmmanuelOdedele <emmaodedele@gmail.com>
2023-04-04 15:38:42 +0100

    fetching data

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" :key="post.id">
-      <h3>{{ post.title }}</h3>
+      <SinglePost :post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import SinglePost from "./SinglePost.vue";
+import { onMounted, onUnmounted, onUpdated } from "vue";
+
 export default {
+  components: { SinglePost },
   props: ["posts"],
   setup(props) {
-    console.log(props.posts)
-  }
+    onMounted(() => console.log('mounted'))
+    onUnmounted(()=>console.log('unmounted'))
+    onUpdated(()=>console.log('updated'))
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList v-if="showPosts" :posts="posts" />
+    </div>
+    <div v-else>Loading..</div>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
 
     <!-- <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click Me</button>
@@ -71,10 +77,24 @@ export default {
     // })
 
     // for props
-    const posts = ref([
-      { title: "welcome to the blog", body: "Lorem ipsum", id: 1 },
-      { title: "top 5 CSS tips", body: "Lorem ipsum", id: 2 },
-    ]);
+    const posts = ref([]);
+    const showPosts = ref(true);
+    const error = ref(null);
+
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("no data available");
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
+    };
+
+    load();
 
     return {
       // name,
@@ -89,6 +109,8 @@ export default {
       // matchingNames
 
       posts,
+      showPosts,
+      error,
     };
   },
 };
